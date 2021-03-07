b496e826057e76d1087d40247c82bfa8ddf9d5cb
Tawanda Mavondo <tawandagdmavondo@gmail.com>
2021-03-07 09:14:10 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QueryPostsGQL } from './services/wordpressGraphQl.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   isCollapsed = false;
+
+  constructor(private postsService: QueryPostsGQL) { }
+
+  public posts$;
+  ngOnInit(): void {
+    this.posts$ = this.postsService.fetch().pipe(
+      map(
+        res => res.data.posts
+      ));
+    // console.log(this.posts$)
+    console.log('init');
+  }
+  log(post): void {
+    console.log(post);
+  }
 }
