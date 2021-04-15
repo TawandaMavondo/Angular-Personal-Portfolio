9dc8dac7eee9d9c5d1416f2a629a0003af38ff7c
Tawanda Mavondo <tawandagdmavondo@gmail.com>
2021-04-15 16:26:40 +0200

    .

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import { BannerModule } from '../components/banner/banner.module';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, AboutComponent],
   imports: [
     CommonModule,
     BannerModule
