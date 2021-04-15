6312f95b0ee41c2ed930b8b4f96812c65d56f408
Tawanda Mavondo <tawandagdmavondo@gmail.com>
2021-04-15 16:03:16 +0200

    .

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as typed from 'typed.js';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
-
+  private readonly strings = [
+    'I build things for the Web.',
+    'I build software for fun.',
+    'I build scalable backend services.'
+  ];
   constructor() { }
 
   ngOnInit(): void {
+    const options: typed.TypedOptions = {
+      strings: this.strings,
+      backSpeed: 50,
+      typeSpeed: 100,
+      backDelay: 500,
+      showCursor: false,
+      cursorChar: '|',
+      loop: true
+    }
+    const typedE = new typed.default('.typing', options);
   }
 
 }
--- src/app/components/banner/banner.module.ts
+++ src/app/components/banner/banner.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner.component';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 
 
 @NgModule({
   declarations: [BannerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxTypedJsModule
   ],
   exports: [BannerComponent]
 })
