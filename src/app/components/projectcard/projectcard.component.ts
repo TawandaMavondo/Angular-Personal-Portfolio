5163bd2c13af68f201becea05612440f4dfbd232
Tawanda Mavondo <tawandagdmavondo@gmail.com>
2021-04-27 14:46:50 +0200

    .

--- src/app/components/projectcard/projectcard.component.ts
+++ src/app/components/projectcard/projectcard.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {  faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input, OnInit } from '@angular/core';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-projectcard',
@@ -9,6 +9,31 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 export class ProjectcardComponent implements OnInit {
   public faExternalLinkAlt = faExternalLinkAlt;
   public faGithub = faGithub;
+
+  @Input()
+  public reverse: boolean;
+
+  @Input()
+  public technologies: string;
+
+  @Input()
+  public description: string;
+
+  @Input()
+  public title: string;
+
+  @Input()
+  public githubLink: string;
+
+
+  @Input()
+  public externalLink: string;
+
+  @Input()
+  public imageUrl: string;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
