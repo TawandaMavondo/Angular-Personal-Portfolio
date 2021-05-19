fb2e5a099bf6e8e25e2cf7f9a8f65f63d9a0cf41
Tawanda <tawandagdmavondo@gmail.com>
2021-05-19 19:06:56 +0200

    .

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { StringValueNode } from 'graphql';
 
+type NavItem = {
+  name: string,
+  id: string
+}
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
+
 export class LayoutComponent implements OnInit {
   isCollapsed = false;
+  public navItems: NavItem[] = [
+    {
+      name: 'About Me',
+      id: 'about-section'
+    },
+    {
+      name: 'Experince',
+      id: 'experience-section'
+    },
+    {
+      name: 'Portfolio',
+      id: 'portfolio-section'
+    },
+    {
+      name: 'Contact',
+      id: 'contact-section'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public scrollToSection(id: string): void {
+
+    const element: HTMLElement = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
   }
 
 }
