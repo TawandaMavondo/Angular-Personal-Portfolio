1cf0ad274a3f7fe052aa68860f54c1ca6293280e
Tawanda <tawandagdmavondo@gmail.com>
2022-01-02 09:28:22 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { QueryPostsGQL, QueryPostsQuery } from './services/wordpressGraphQl.service';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs'
-import { ApolloQueryResult } from '@apollo/client/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
-  constructor(private postsService: QueryPostsGQL) { }
+  constructor() {}
 
   isCollapsed = false;
 
   public posts$: any;
 
-
-
-  ngOnInit(): void {
-    //   this.posts$ = this.postsService.fetch().pipe(
-    //     map(
-    //       res => res.data.posts?.nodes
-    //     ));
-    // }
-    // log(post): void {
-    //   console.log(post);
-    // }
-  }
+  ngOnInit(): void {}
 }
